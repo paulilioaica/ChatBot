79ff29c57c50dc451255893a2b1b7ce713dae588
Paul <paulicapopcorn@gmail.com>
2019-10-08 19:27:17 +0300

    Added cuda capabilities

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import torch.nn as nn
 from chatbot import *
 from dataloader import get_training_data
 
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 max_target_len = 10
 batch_size = 4
 
-model = ChatBot(hidden_size=256, vocab_size=68215, output_size=68215, n_layers=1, dropout=0)
+model = ChatBot(hidden_size=256, vocab_size=68215, output_size=68215, n_layers=1, dropout=0).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
-encoder_hidden = torch.zeros(2, 4,256)
-decoder_input  = torch.zeros(1, 4, 256)
-decoder_hidden = torch.zeros(1, 4, 256)
-input_variable = torch.zeros((100, 4)).long()
+encoder_hidden = torch.zeros(2, 4,256).to(device)
+decoder_input  = torch.zeros(1, 4, 256).to(device)
+decoder_hidden = torch.zeros(1, 4, 256).to(device)
+input_variable = torch.zeros((100, 4)).long().to(device)
 X_train, Y_train = get_training_data(batch_size=4)
 for epoch in range(0, 6000):
     for iteration in range(X_train.shape[0]):
-        input_variable = torch.tensor(X_train[iteration]).transpose(1,0).long()
-        global_target = torch.tensor(Y_train[iteration]).long()
+        input_variable = torch.tensor(X_train[iteration]).transpose(1,0).long().to(device)
+        global_target = torch.tensor(Y_train[iteration]).long().to(device)
         encoder_outputs, encoder_hidden = model.encoder(input_variable, encoder_hidden)
         optimizer.zero_grad()
         loss_sum = 0
